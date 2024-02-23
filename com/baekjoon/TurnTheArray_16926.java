8d0fe09498c0572ef4015ccd4439dac65d06b230
Kwak-98 <64453121+Kwak-98@users.noreply.github.com>
2024-02-23 13:50:01 +0900

    [UPDATE]TurnTheArray_16926.java
    맞았습니다!

--- com/baekjoon/TurnTheArray_16926.java
+++ com/baekjoon/TurnTheArray_16926.java
@@ -5,76 +5,58 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class TurnTheArray_16926 {
 
 	public static void main(String[] args) throws IOException {
 		
-		System.out.println("시작");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
 		
-		//BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		//StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+		// N x M 배열, 회전수 R
+		int N = Integer.parseInt(tokenizer.nextToken());	
+		int M = Integer.parseInt(tokenizer.nextToken());
+		int R = Integer.parseInt(tokenizer.nextToken());
 		
-//		// N x M 배열, 회전수 R
-//		int N = Integer.parseInt(tokenizer.nextToken());	
-//		int M = Integer.parseInt(tokenizer.nextToken());
-//		int R = Integer.parseInt(tokenizer.nextToken());
-//		
-//		// N과 M이 둘다 홀수 또는 짝수,홀수이면서 홀수가 더 작을경우 배열 회전 불가능
-//		
-//		int[][] arr = new int[N][M];
-//		
-//		// 배열의 요소 입력받기
-//		for(int i = 0; i < N; i++) {
-//			
-//			tokenizer = new StringTokenizer(reader.readLine());
-//			
-//			for(int j = 0; j < M; j++) {
-//				arr[i][j] = Integer.parseInt(tokenizer.nextToken());
-//				
-//			}
-//			
-//		}//입력받기 끝
+		// N과 M이 둘다 홀수 또는 짝수,홀수이면서 홀수가 더 작을경우 배열 회전 불가능
 		
-//		//테스트 케이스1
-//		int N = 4;
+		int[][] arr = new int[N][M];
+		
+		// 배열의 요소 입력받기
+		for(int i = 0; i < N; i++) {
+			
+			tokenizer = new StringTokenizer(reader.readLine());
+			
+			for(int j = 0; j < M; j++) {
+				arr[i][j] = Integer.parseInt(tokenizer.nextToken());
+				
+			}
+			
+		}//입력받기 끝
+		
+//		// 테스트 케이스
+//		int N = 5;
 //		int M = 4;
-//		int R = 2;
-//		
-//		int[][] arr = { {1, 2, 3, 4},
-//						{5, 6, 7, 8},
-//						{9, 10, 11, 12},
-//						{13, 14, 15, 16} };
+//		int R = 7;
+//		int[][] arr = {{1, 2, 3, 4},{7, 8, 9, 10,},{13, 14, 15, 16},{19, 20, 21, 22},{25, 26, 27, 28}};
 		
-		// 테스트 케이스2
-		int N = 5;
-		int M = 4;
-		int R = 7;
+		arr = turnTheArray(arr, R);	// 배열 R번 회전
 		
-		int[][] arr = { {1, 2, 3, 4},
-					    {7, 8, 9, 10},
-					    {13, 14, 15, 16},
-				        {19, 20, 21, 22},
-				        {25, 26, 27, 28} };
-		
-		// 2차원 배열과 회전수를 매개변수로
-		// R번 반시계방향으로 회전
-		arr = turnTheArray(arr, R);	
-		
-		
-		// 배열 출력
-		printArr(arr);
-	
+		printArr(arr);				// 배열 출력
 		
 	}//main
 
-
+	
+	/**
+	 * 배열을 R만큼 회전시키는 메서드
+	 * @param arr	: 입력 2차원배열
+	 * @param R		: 회전수
+	 * @return		: 회전된 배열 반환
+	 */
 	private static int[][] turnTheArray(int[][] arr, int R) {
 		
 		for(int k = 0; k < Math.min(arr.length, arr[0].length) / 2; k++) {	//레이어 수만큼 반복
-			
 			StringBuilder builder = new StringBuilder();
 			
 			for(int i = k; i < arr.length - k; i++) {						// 레이어의 왼쪽
@@ -91,19 +73,16 @@ public class TurnTheArray_16926 {
 			for(int i = (arr[0].length - 1) - 1 - k; i > k; i--) {			// 레이어의 위
 				builder.append(arr[k][i] + ".");
 			}
-			
 			// 레이어를 '.'으로 연결된 문자열 builder 완료
-			System.out.println(builder.toString());
 			
-			// 레이어 문자열을 회전시키는 메서드에 인자로 
-			// 회전된 문자열을 tokenizer에 token화
-			StringTokenizer tokenizer = new StringTokenizer(turnning(builder, R), ".");
+			StringTokenizer tokenizer = new StringTokenizer(turnning(builder, R), ".");		// 회전된 레이어 문자열
 			
 			
 			// 다시 레이어의 값에 tokenizer를 순차적으로 삽입
 			for(int i = k; i < arr.length - k; i++) {						// 레이어의 왼쪽
 				arr[i][k] = Integer.parseInt(tokenizer.nextToken());
 			}
+			
 			for(int i = k + 1; i < arr[0].length - k; i++) {				// 레이어의 아래
 				arr[(arr.length - 1) - k][i] = Integer.parseInt(tokenizer.nextToken());
 			}
@@ -124,11 +103,16 @@ public class TurnTheArray_16926 {
 	}//turnTheArray()
 	
 	
-	
+	/**
+	 * 레이어 문자열 나눠서 회전수만큼 
+	 * @param builder	: 레이어 문자열
+	 * @param R			: 회전수
+	 * @return			: 회전된 레이어 문자열
+	 */
 	 private static String turnning(StringBuilder builder, int R) {
 		 
-	        String[] arr = builder.toString().split("\\."); // 
-	        int turnNum = R % arr.length;
+	        String[] arr = builder.toString().split("\\.");		// 
+	        int turnNum = R % arr.length;						// 회전수 % 레이어요소의 개수 = 회전수
 	        int length = arr.length;
 	        
 	        if(turnNum == 0) {
@@ -137,6 +121,7 @@ public class TurnTheArray_16926 {
 	        
 	        // 뒤에서부터 n개의 값을 가져와서 새로운 문자열을 만듦
 	        builder = new StringBuilder();
+	        
 	        for (int i = length - turnNum; i < length; i++) {
 	            builder.append(arr[i] + ".");
 	        }
@@ -145,12 +130,11 @@ public class TurnTheArray_16926 {
 	            builder.append(arr[i] + ".");
 	        }
 	        
-	        System.out.println(builder.toString());
 	        return builder.toString();
 	 }//turnning()
 	 
 	 
-	 
+	 // 2차원 배열출력
 	 private static void printArr(int[][] arr) {
 		 BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 		 
