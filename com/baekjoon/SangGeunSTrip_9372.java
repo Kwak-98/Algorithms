3c9f4a9ad2b9ae9db4d0732274ffca16a3e05cae
Kwak-98 <64453121+Kwak-98@users.noreply.github.com>
2024-04-02 23:25:08 +0900

    [UPDATE]Tree문제

--- com/baekjoon/SangGeunSTrip_9372.java
+++ com/baekjoon/SangGeunSTrip_9372.java
@@ -1,11 +1,49 @@
 package com.baekjoon;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
 public class SangGeunSTrip_9372 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder builder = new StringBuilder();
+		StringTokenizer tokenizer;
 		
+		int T = Integer.parseInt(reader.readLine());
 		
-	}
+		for(int i = 0; i < T; i++) {
+			
+			tokenizer = new StringTokenizer(reader.readLine());
+			
+			int N = Integer.parseInt(tokenizer.nextToken());
+			int M = Integer.parseInt(tokenizer.nextToken());
+			HashSet<String> set = new HashSet<>();
+			
+			for(int j = 0; j < M; j++) {
+				tokenizer = new StringTokenizer(reader.readLine());
+				set.add(tokenizer.nextToken());
+				set.add(tokenizer.nextToken());
+			}
+			builder.append(set.size() - 1).append("\n");
+			
+		}
+		
+		builder.deleteCharAt(builder.length() - 1);
+		
+		writer.write(builder.toString());
+		writer.flush();
+			
+		writer.close();
+		reader.close();
+		
+	}//main
 
 }//End of class
--- com/baekjoon/TreeRounds_1991.java
+++ com/baekjoon/TreeRounds_1991.java
@@ -5,37 +5,65 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
+import java.util.StringJoiner;
 import java.util.StringTokenizer;
 
 public class TreeRounds_1991 {
 	
-	public static StringBuilder builder;
-
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-		Stack<String> stack = new Stack<>();
 		StringTokenizer tokenizer;
+		
+		HashMap<String, Node> map = new HashMap<>(); 
+		
+		Tree tree = new Tree();
+		Node root = new Node("A");
+		tree.setRoot(root);
+		map.put(root.value, root);
 		
 		int N = Integer.parseInt(reader.readLine());
 		
-		
-		
 		for(int i = 0; i < N; i++) {
+			tokenizer = new StringTokenizer(reader.readLine());
+			
+			Node paNode = map.get(tokenizer.nextToken());
+			String left = tokenizer.nextToken();
+			String right = tokenizer.nextToken();
+			
+			
+			if(left.equals(".")) {
+				paNode.left = null;
+			}else {
+				Node lNode = new Node(left);
+				map.put(left, lNode);
+				paNode.left = lNode;
+			}
+			
+			if(right.equals(".")) {
+				paNode.right = null;
+			}else {
+				Node rNode = new Node(right);
+				map.put(right, rNode);
+				paNode.right = rNode;
+			}
 			
 		}
 		
-		Tree tree = new Tree();
+		tree.preOrder(tree.getRoot());
+		Tree.builder.append("\n");
+		tree.inOrder(tree.getRoot());
+		Tree.builder.append("\n");
+		tree.postOrder(tree.getRoot());
 		
-		
-		
-		
-		
-		writer.write(builder.toString());
+		writer.write(Tree.builder.toString());
+		writer.flush();
 			
 		writer.close();
 		reader.close();
@@ -50,17 +78,16 @@ class Node {
 	public Node right;
 	public Node left;
 	
-	public Node(String value, Node left, Node right) {
+	public Node(String value) {
 		this.value = value;
-		this.left = left;
-		this.right = right;
 	}
+	
 	
 }//End of Node
 
 class Tree{
 	
-	public static Queue<String> queue = new LinkedList<>();
+	public static StringBuilder builder = new StringBuilder();
 	
 	public Node root;
 
@@ -74,43 +101,27 @@ class Tree{
 	
 	public void preOrder(Node node) {	//전위 순회
 		if(node != null) {
-			queue.add(node.value);
+			builder.append(node.value);
 			preOrder(node.left);
 			preOrder(node.right);
 		}
-	}
+	}//preOder()
 	
 	public void inOrder(Node node) {		// 중위 순회
 		if(node != null) {
 			inOrder(node.left);
-			queue.add(node.value);
+			builder.append(node.value);
 			inOrder(node.right);
 		}
-	}
+	}//inOrder()
 	
 	public void postOrder(Node node) {	// 후위 순회
 		if(node != null) {
 			postOrder(node.left);
 			postOrder(node.right);
-			queue.add(node.value);
+			builder.append(node.value);
 		}
-	}
+	}//postOrder()
 	
 }//End of Tree
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
